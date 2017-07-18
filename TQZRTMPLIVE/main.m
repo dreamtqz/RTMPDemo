9c17be799f467e139a7314f5e9c0ad887b628023
汤庆泽 <tangqingze@kdnet.net>
2017-07-18 16:04:25 +0800

    Initial Commit

---
+++ TQZRTMPLIVE/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TQZRTMPLIVE
+//
+//  Created by 汤庆泽 on 17/7/18.
+//  Copyright © 2017年 海南凯迪网络有限公司广州分公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
